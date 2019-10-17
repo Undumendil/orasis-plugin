ea9ea0fe111393b3eb5ecdff6d932c4bdfeb380f
Undumendil <azathtoth@protonmail.com>
2019-10-17 15:39:18 +0300

    API update

--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,41 @@
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
+use std::sync::mpsc::{Receiver, Sender};
+
 pub struct EventData {
     /// Plugin intended to receive the event. Use `None` to trigger processing by plugin manager.
     pub target_id: Option<String>,
     /// A short string describing type of event.
     pub meta: String,
     /// Optional `String` receiver.
-    pub message: Option<std::sync::mpsc::Receiver<String>>,
+    pub message: Option<Receiver<String>>,
     /// Optional `u64` receiver.
-    pub data: Option<std::sync::mpsc::Receiver<u64>>
+    pub data: Option<Receiver<u64>>
+}
+
+impl EventData {
+    pub fn sys(meta: String,
+               message: Option<Receiver<String>>,
+               data: Option<Receiver<u64>>) -> Self {
+        EventData {
+            target_id: None,
+            meta,
+            message,
+            data
+        }
+    }
+    pub fn new(target_id: String,
+               meta: String,
+               message: Option<Receiver<String>>,
+               data: Option<Receiver<u64>>) -> Self {
+        EventData {
+            target_id: Some(target_id),
+            meta,
+            message,
+            data
+        }
+    }
 }
 
 pub enum Event {
@@ -32,23 +58,23 @@ pub enum Event {
 pub struct VirtualCanvas<'a> {
     pub width: u64,
     pub height: u64,
-    /// ARGB data; access pixels with [y * width + x].
+    /// ARGB data; access pixels with `[y * width + x]`.
     pub data: &'a mut [u32],
 
     pub o_width: u64,
     pub o_height: u64,
-    /// Overlay as visible on the screen; access pixels with [y * o_width + x].
+    /// Overlay as visible on the screen; access pixels with `[y * o_width + x]`.
     pub overlay: &'a mut [char]
 }
 
-pub trait ImageEditorPlugin : Send {
+pub trait ImageEditorPlugin {
     /// Store emitter to be able to send events later.
-    fn setup(&mut self, emitter: std::sync::mpsc::Sender<&Event>);
+    fn setup(&mut self, emitter: Sender<&Event>);
     
     /// Plugin-unique identifier. May be used in `EventData.target_id`.
     fn id(&self) -> String;
     
-    /// Plugin is starting to be used.
+    /// Plugin is starting to be used. Called by user, not other plugins.
     fn activate(&mut self, canvas: &VirtualCanvas);
     
     /// Plugin may draw some additional overlay there.
@@ -57,6 +83,6 @@ pub trait ImageEditorPlugin : Send {
     /// Return value: whether this plugin consumes the event.
     fn act(&mut self, ev: &Event, canvas: &mut VirtualCanvas) -> bool;
     
-    /// Plugin is deactivated. It may be activated again later.
+    /// Plugin is deactivated. It may be activated again later. Called by user, not other plugins.
     fn deactivate(&mut self);
 }
